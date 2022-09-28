1abcc3d2008d6e3c6a94fdffc7d45e92e7542e29
hugopukito <gogo26hugop@gmail.com>
2022-09-28 19:12:11 +0200

    add gorilla mux for router

--- router/router.go
+++ router/router.go
@@ -1,13 +1,18 @@
 package router
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"module.com/webServer/service"
 )
 
 func InitRouter() {
-	http.HandleFunc("/", service.GetUsers)
-	http.HandleFunc("/post", service.PostUserMessage)
-	http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/users", service.GetUsers).Methods("GET")
+	router.HandleFunc("/users", service.PostUserMessage).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
